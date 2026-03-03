7761a60b61f5b374bb707b08783ea5fc261d0432
marcogrena <33692969+marcogrena@users.noreply.github.com>
2026-03-03 10:38:07 +0000

    add examples c++ and .gitignore

--- client c++/request.cpp
+++ client c++/request.cpp
@@ -17,13 +17,15 @@ int main(void)
 
   curl = curl_easy_init();
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000/prefix?prefix=arzi&light=true");
+    curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:3000/prefix?prefix=bad&light=true&lang=en");
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
     res = curl_easy_perform(curl);
     curl_easy_cleanup(curl);
 
     std::cout << readBuffer << std::endl;
+    if (readBuffer=="[]")
+    std::cout<<"NON CI SONO PAROLE";
   }
   return 0;
 }
